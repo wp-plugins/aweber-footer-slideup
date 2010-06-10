1b3850d9e6b3767df4ed12829a0810918a5d01e5
shabbirbhimani <shabbirbhimani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-10 16:17:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/aweber-footer-slideup/trunk@250636 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- awfs.php
+++ awfs.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: AWeber Footer SlideUp
 	Plugin URI: http://wordpress.org/extend/plugins/aweber-footer-slideup/
-	Description: Installs AWeber Footer Form
+	Description: Footer Slideup Form is one of the best way to ask your user to subscribe to your list without any interruption or blocking and this plugin does exactly that.  It adds an AWeber subscribe form in the footer of your Wordpress blog. <A HREF="http://g4ef.aweber.com">Aweber</A> (Aff Link) is one of the better  autoresponders in the market and it allows you to send series of email message to subscribers. You can read my detailed <A  HREF="http://www.codeitwell.com/aweber-autoresponder-review.html" target="_blank">AWeber review</A> on my personal blog.
 	Author: Shabbir Bhimani
 	Version: 0.1-alpha
 	Author URI: http://www.codeitwell.com/
@@ -28,7 +28,6 @@ $plugin_abs_url = WP_PLUGIN_URL.'/aweber-footer-slideup';
 }
 
 function awfs_form()  {
-$plugin_abs_path = WP_PLUGIN_DIR.'/aweber-footer-slideup';
 ?>
 <div id="footerform">
 	<div class="close">
@@ -36,19 +35,69 @@ $plugin_abs_path = WP_PLUGIN_DIR.'/aweber-footer-slideup';
 	    <div id="dontshowanymore"><a href="#" onclick="slidedown(); return false;">Never show again.</a></div>
 	</div>
 
-	<div class="tagline"><?php include ($plugin_abs_path . '/awfs-tagline.php') ?></div>
+	<div class="tagline"><?php $awfs_tagline=get_option('awfs_tagline'); echo $awfs_tagline==''?'Subscribe By Email for Weekly Updates.':$awfs_tagline; ?></div>
 
 	<form method="post" action="http://www.aweber.com/scripts/addlead.pl" target="_new">
-		<?php include ($plugin_abs_path . '/awfs-hiddenfields.php') ?>
+		<?php echo get_option('awfs_hiddenfields'); ?>
 		<input type="text" name="name" class="formInputfooter formInputNamefooter" value="What is your first name?" size="20" />
 		<input type="text" name="from" class="formInputfooter formInputEmailfooter" value="What is your email?" size="20" />
 		<input type="submit" name="submit" class="formInputSubmitfooter" value="Subscribe Now !!!">
 	</form>
-
-	<img src="http://forms.aweber.com/form/displays.htm?id=LMxsHAwcDKys" border="0" />
+<?php $awfs_formstatstracking= get_option('awfs_formstatstracking'); if(awfs_formstatstracking!='') echo "<img src='$awfs_formstatstracking' border='0' />"; ?>
 </div>
 <?
 }
 add_action ( 'wp_footer', 'awfs_form');
 add_action ( 'wp_head', 'awfs_header_elements');
+add_action ( 'admin_menu', 'awfs_plugin_menu');
+
+function awfs_plugin_menu() {
+
+	add_options_page('My Plugin Options', 'AWeber Footer SlideUp', 'manage_options', 'awfs', 'awfs_plugin_options');
+	add_action( 'admin_init', 'register_awfs_settings' );
+}
+
+function register_awfs_settings() {
+	//register our settings
+	register_setting( 'awfs-settings-group', 'awfs_tagline' );
+	register_setting( 'awfs-settings-group', 'awfs_hiddenfields' );
+}
+
+
+function awfs_plugin_options() {
+?>
+<div class="wrap">
+<p>Footer Slideup Form is one of the best way to ask your user to subscribe to your list without any interruption or blocking and this plugin does exactly that. It adds an AWeber subscribe form in the footer of your Wordpress blog. <A HREF="http://g4ef.aweber.com">AWeber</A> (Aff Link) is one of the better autoresponders in the market and it allows you to send series of email message to subscribers. You can read my detailed <A HREF="http://www.codeitwell.com/aweber-autoresponder-review.html" target="_blank">AWeber review</A> on my personal blog.</p>
+<p>If you are still not using AWeber for your blog and thinking of purchase you can use my affiliate <A HREF="http://g4ef.aweber.com/">link</A>.</p>
+<p>For all your queries, help and support for plugin please post them in comments <A HREF="http://www.codeitwell.com/aweber-footer-slideup/" target="_blank">at my blog</A>. I will be actively supporting the plugin.</p>
+
+<form method="post" action="options.php">
+<?php settings_fields( 'awfs-settings-group' ); ?>
+<table class="form-table">
+<tr valign="top">
+<th scope="row">Heading Tag Line (Optional)</th>
+<td><input type="text" name="awfs_tagline" value="<?php echo get_option('awfs_tagline'); ?>" /></td>
+</tr>
+<tr valign="top">
+<th scope="row">Tracking Image URL of AWeber Webform (Optional)</th>
+<td><input type="text" name="awfs_formstatstracking" value="<?php echo get_option('awfs_formstatstracking'); ?>" /></td>
+</tr>
+<tr valign="top">
+<th scope="row">Hidden AWeber Form Fields (Required)
+<div style="font-weight:bold;color:red;padding-top:10px;">Generate an Inline form using the AWeber WebForm Interface. Grab the HTML code of your AWeber Form and Search for Hidden Div which lists all the hidden form fields. You will see it just after after the <FORM> tag.
+</div></th>
+<td>
+<textarea name="awfs_hiddenfields" id="awfs_hiddenfields" rows="15" cols="90"  ><?php echo get_option('awfs_hiddenfields'); ?></textarea>
+</td>
+</tr>
+</table>
+<input type="hidden" name="action" value="update" />
+<input type="hidden" name="page_options" value="awfs_formstatstracking, awfs_tagline,awfs_hiddenfields" />
+<p class="submit">
+<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+</p>
+</form>
+</div>
+<?
+}
 ?>
--- readme.txt
+++ readme.txt
@@ -1,33 +1,35 @@
 === Plugin Name ===
 Contributors: shabbirbhimani
 Tags: AWeber, Footer Form, SLideUp
-Requires at least: 2.9
+Requires at least: 2.8
 Tested up to: 2.9.2
 Stable tag: trunk
 
-Footer Slideup Form adds an AWeber form into the footer and user can subscribe to it.
+Footer Slideup Form adds an AWeber Opt-In form into the footer where user can subscribe to it.
 
 == Description ==
 
-Footer Slideup Form is one of the best way to ask your user to subscribe to your list without any interruption or blocking and this plugin does exactly that. It adds an AWeber form into the footer and user has couple of options apart from subscribing and that is either close the plugin for the current session or for rest of his life. The information is stored in browser cookies and if user clears them he will see the form again.
+Footer Slideup Form is one of the best way to ask your user to subscribe to your list without any interruption or blocking and this plugin does exactly that. It adds an AWeber subscribe form in the footer of your Wordpress blog. <A HREF="http://g4ef.aweber.com">AWeber</A> (Aff Link) is one of the better autoresponders in the market and it allows you to send series of email message to subscribers. You can read my detailed <A HREF="http://www.codeitwell.com/aweber-autoresponder-review.html" target="_blank">AWeber review</A> on my personal blog.
 
 == Installation ==
 
 Please Follow the exact Steps to Install this plugin.
 
 e.g.
-1. Download aweber-footer-slideup Plugin to your Local PC.
-1. Generate an AWeber Inline form using the AWeber Web Form Interface. Use any basic theme as the theme you select in AWeber will not be used.
+1. Generate an Inline form using the AWeber Web form Interface.
 1. Grab the HTML code of your AWeber Form and Search for Hidden Div which lists all the hidden form fields. You will see it after the <FORM> tag.
-1. Open the file aweber-footer-slideup/awfs-hiddenfields.php in notepad and copy the hidden Div and paste into the file. Save the file now.
-1. Upload `aweber-footer-slideup` to the `/wp-content/plugins/` directory
 1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Edit Options under the Menu AWeber Footer SlideUp and the hidden div HTML Code into Text Box and Save the options.
 
 == Frequently Asked Questions ==
 
 = How to Edit the Tag Line Subscribe By Email for Weekly Updates? =
 
-I have kept it to simple file edit aweber-footer-slideup\awfs-tagline.php as of now but soon I will be adding admin interface into the plugin.
+Heading Tag Line (Optional) in the admin interface of the plugin. Leaving it blank will show the default tagline.
+
+= What is Tracking Image URL of AWeber Webform (Optional)? = 
+
+AWeber RAW HTML webform tracks the form view with an image and if you see just near the form end tag you will see <img src="SomeValue" border="0" />. Just copy the SomeValue from the HTML code and the image url in the Tracking Image URL of AWeber Webform.
 
 == Screenshots ==
 
@@ -35,10 +37,14 @@ I have kept it to simple file edit aweber-footer-slideup\awfs-tagline.php as of 
 
 == Changelog ==
 
+= 0.2 =
+* Basic Working Version of AWeber Footer Slideup with Admin Options Now Added.
+
+
 = 0.1 =
 * Basic Working Version of AWeber Footer Slideup
 
 == Upgrade Notice ==
 
-= 0.1 =
+= 0.2 =
 This is the initial version and so there is nothing to upgrade.
